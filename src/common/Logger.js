0c09853ff1b8b8c331c10e77b9fa46dc44144c97
whiteFang <jpm8888@gmail.com>
2021-07-17 14:11:07 +0530

    lint fixed

--- src/common/Logger.js
+++ src/common/Logger.js
@@ -1,25 +1,25 @@
-/* eslint-disable */
 const chalk = require('chalk');
 
-
-const logError = (TAG, message) =>{
-    console.log(chalk.bold.red(TAG, message));
-}
-const logSuccess = (TAG, message) =>{
-    console.log(chalk.bold.green(TAG, message));
-}
-const logWarning = (TAG, message) =>{
-    console.log(chalk.bold.yellow(TAG, message));
-}
-const logInfo = (TAG, message) =>{
-    console.log(chalk.bold.blueBright(TAG, message));
-}
-
-module.exports = {
-    logError, logSuccess,
-    logWarning, logInfo
+const logError = (TAG, message) => {
+  // eslint-disable-next-line no-console
+  console.log(chalk.bold.red(TAG, message));
+};
+const logSuccess = (TAG, message) => {
+  // eslint-disable-next-line no-console
+  console.log(chalk.bold.green(TAG, message));
+};
+const logWarning = (TAG, message) => {
+  // eslint-disable-next-line no-console
+  console.log(chalk.bold.yellow(TAG, message));
+};
+const logInfo = (TAG, message) => {
+  // eslint-disable-next-line no-console
+  console.log(chalk.bold.blueBright(TAG, message));
 };
 
-
-
-
+module.exports = {
+  logError,
+  logSuccess,
+  logWarning,
+  logInfo,
+};
