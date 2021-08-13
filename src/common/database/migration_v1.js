c4a735b125124759cf43ba81b63d2a4006e46707
jpm <jpm8888@gmail.com>
2021-08-13 21:28:10 +0530

    - added new migration table.

--- src/common/database/migration_v1.js
+++ src/common/database/migration_v1.js
@@ -20,6 +20,30 @@ const create_table_prefs = async (db) => {
   }
 };
 
+const create_table_instruments = async (db) => {
+  const table_name = 'instruments';
+  let query = `create table if not exists ${table_name}(`;
+  query += 'token text, ';
+  query += 'symbol text, ';
+  query += 'name text, ';
+  query += 'expiry text, ';
+  query += 'strike real, ';
+  query += 'lotsize integer, ';
+  query += 'instrumenttype text, ';
+  query += 'exch_seg text, ';
+  query += 'tick_size real)';
+
+  try {
+    await db.run(query, []);
+    await Database.create_index(db, table_name, 'token');
+    await Database.create_index(db, table_name, 'symbol');
+    await Database.create_index(db, table_name, 'exch_seg');
+    Logger.logInfo(TAG, `created ${table_name} success`);
+  } catch (error) {
+    Logger.logInfo(TAG, `creation error on ${table_name}${JSON.stringify(error)}`);
+  }
+};
+
 const run_migration_v1 = async () => {
   const db = await Database.getDatabase();
   const is_registered = await migration.is_migration_registered(db, migration_name);
@@ -29,6 +53,7 @@ const run_migration_v1 = async () => {
   }
 
   await create_table_prefs(db);
+  await create_table_instruments(db);
 
   await migration.register_migration(db, migration_name);
 };
