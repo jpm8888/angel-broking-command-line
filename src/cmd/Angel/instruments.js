28a8cc871114cc70f2540eecc3250d1e34411f83
jpm <jpm8888@gmail.com>
2021-08-15 19:46:44 +0530

    - minor fix.

--- src/cmd/Angel/instruments.js
+++ src/cmd/Angel/instruments.js
@@ -1,6 +1,11 @@
 const cliProgress = require('cli-progress');
 const { from } = require('rxjs');
 const { map } = require('rxjs/operators');
+const { ajax } = require('rxjs/ajax');
+const { of } = require('rxjs');
+const { mergeMap } = require('rxjs');
+const { catchError } = require('rxjs');
+const { concatAll } = require('rxjs');
 const Logger = require('../../common/Logger');
 const Database = require('../../common/database/Database');
 const network = require('../../common/NetworkOps');
@@ -38,21 +43,38 @@ const dump_stocks = async () => {
   const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
   const db = await Database.getDatabase();
   try {
-    const res = await network.makeGetRequest(Config.ANGEL_URLS.tradedStocks);
-    progress.start(res.length, 0);
+    // const res = await network.makeGetRequest(Config.ANGEL_URLS.tradedStocks);
+    // progress.start(res.length, 0);
+    // from(truncateDatabase(db);)
+    // await truncateDatabase(db);
+    from(network.makeGetRequest(Config.ANGEL_URLS.tradedStocks))
+      .pipe(
+        mergeMap((data) => {
+          progress.start(data.length, 0);
+          return of(data).pipe(
+            map((item) => from(insert(db, item)).pipe(
+              map(() => progress.increment()),
+            )),
+          );
+        }),
+        catchError((e) => {
+          Logger.logError(TAG, e);
+        }),
+        concatAll(),
+      ).subscribe({
+        complete: () => { console.log('hello'); },
+      });
 
-    await truncateDatabase(db);
-
-    from(res).pipe(
-      map((item) => from(insert(db, item))),
-    ).subscribe(() => {
-      progress.increment();
-    });
+    // from(res).pipe(
+    //   map((item) => from(insert(db, item))),
+    // ).subscribe(() => {
+    //   progress.increment();
+    // });
   } catch (e) {
     console.log(e);
   }
-  await Database.closeDatabase(db);
-  progress.stop();
+  // await Database.closeDatabase(db);
+  // progress.stop();
 };
 
 const commandUpdateInstruments = {
