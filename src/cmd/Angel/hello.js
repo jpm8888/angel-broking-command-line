1f3da6b1e692c553199e9f5540fb7a7d0aaf15db
whiteFang <jpm8888@gmail.com>
2021-08-21 11:36:47 +0530

    - added more samples in hello.js

--- src/cmd/Angel/hello.js
+++ src/cmd/Angel/hello.js
@@ -1,11 +1,27 @@
 const Logger = require('../../common/Logger');
+const Utils = require('../../common/Util');
 
 const TAG = 'hello: ';
+
+function printMacAddress() {
+  Utils.getMacAddress().then((mac) => {
+    Logger.logInfo(TAG, `mac_address: ${mac}`);
+  });
+}
+
+function runCommand() {
+  Utils.runShellCommand('echo $HOME').then((output) => {
+    Logger.logInfo(TAG, `home_dir: ${output}`);
+  });
+}
+
 module.exports = {
   command: 'sayHello',
   describe: 'it says hello | --name [NAME]',
   handler: (argv) => {
     const name = argv.name || 'boomer';
     Logger.logInfo(TAG, `hello ${name}`);
+    printMacAddress();
+    runCommand();
   },
 };
